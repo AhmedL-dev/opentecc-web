7723d6b5082f5739146674a670b52f8e05eea1cb
Ahmed L <61092437+AhmedL-dev@users.noreply.github.com>
2024-02-10 18:59:00 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-"use client"
 import { type Metadata } from 'next'
-import Head from 'next/head'
+import Head from 'next/head' // Import the Head component
 
 import { RootLayout } from '@/components/RootLayout'
 
@@ -17,7 +16,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <Head>
+        {/* Add the Sonetel chat widget script here */}
         <script async id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="208458439"></script>
+        {/* Here you can add other head elements like meta tags, additional scripts, etc. */}
       </Head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
