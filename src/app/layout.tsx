1a647356e1f57e42b72456651c3c38deff30e0ba
Ahmed L <61092437+AhmedL-dev@users.noreply.github.com>
2024-02-10 19:36:13 +0100

    Update layout.tsx removed sanetel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { type Metadata } from 'next'
-import Head from 'next/head' // Import the Head component
 
 import { RootLayout } from '@/components/RootLayout'
 
@@ -15,11 +14,6 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
-      <Head>
-        {/* Add the Sonetel chat widget script here */}
-        <script async id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="208458439"></script>
-        {/* Here you can add other head elements like meta tags, additional scripts, etc. */}
-      </Head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
